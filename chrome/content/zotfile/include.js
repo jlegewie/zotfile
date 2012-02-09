df5403efa7d5789bea35aedf65997f2509e0d32f
Joscha Legewie <jpl2136@columbia.edu>
2012-02-09 13:58:45 -0500

    change in include.js - fixes #20

--- chrome/content/zotfile/include.js
+++ chrome/content/zotfile/include.js
@@ -1,10 +1,7 @@
 // Only create main object once
-if (!Zotero.ZotFile) {
-	var zotfileLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-					.getService(Components.interfaces.mozIJSSubScriptLoader);
-	zotfileLoader.loadSubScript("chrome://zotfile/content/zotfile.js");
-	window.addEventListener('load', function(e) { Zotero.ZotFile.init(); }, false);
-} 
+Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+				.getService(Components.interfaces.mozIJSSubScriptLoader).loadSubScript("chrome://zotfile/content/zotfile.js");
+window.addEventListener('load', function(e) { Zotero.ZotFile.init(); }, false);
 
 
 
