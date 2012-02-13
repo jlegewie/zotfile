e6b612181f94201367438d2c29cd859ccc530bf8
Joscha Legewie <jpl2136@columbia.edu>
2012-02-13 08:13:16 -0500

    Revert "change in include.js - fixes #20"
    This reverts commit df5403efa7d5789bea35aedf65997f2509e0d32f.

--- chrome/content/zotfile/include.js
+++ chrome/content/zotfile/include.js
@@ -1,7 +1,10 @@
 // Only create main object once
-Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-				.getService(Components.interfaces.mozIJSSubScriptLoader).loadSubScript("chrome://zotfile/content/zotfile.js");
-window.addEventListener('load', function(e) { Zotero.ZotFile.init(); }, false);
+if (!Zotero.ZotFile) {
+	var zotfileLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+					.getService(Components.interfaces.mozIJSSubScriptLoader);
+	zotfileLoader.loadSubScript("chrome://zotfile/content/zotfile.js");
+	window.addEventListener('load', function(e) { Zotero.ZotFile.init(); }, false);
+} 
 
 
 
