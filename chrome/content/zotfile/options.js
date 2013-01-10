ef0fb2874ecd49f4eb0ea33f13bb1f4712d34b12
Joscha Legewie <jpl2136@columbia.edu>
2013-01-10 01:40:37 -0500

    fix FF 18 privacy stuff in saveURI

--- chrome/content/zotfile/options.js
+++ chrome/content/zotfile/options.js
@@ -706,7 +706,13 @@ function downloadPDFTool() {
     
     wbp.progressListener = progressListener;
 //  Zotero.debug("Saving " + uri.spec + " to " + fileURL.spec);
-    wbp.saveURI(uri, null, null, null, null, fileURL);
+    wbp.saveURI(uri, null, null, null, null, fileURL, null);
+    /*try {
+        wbp.saveURI(uri, null, null, null, null, fileURL);
+    } catch(e if e.name === "NS_ERROR_XPC_NOT_ENOUGH_ARGS") {
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=794602
+        wbp.saveURI(uri, null, null, null, null, fileURL, null);
+    }*/
             
 }
       
