4e376a03fcd37058b6875b4022ef23e2155092f4
Joscha Legewie <jpl2136@columbia.edu>
2013-01-21 22:40:29 -0500

    fix for focus handler (watchFolder)

--- chrome/content/zotfile/include.js
+++ chrome/content/zotfile/include.js
@@ -3,12 +3,11 @@ if (!Zotero.ZotFile) {
 	var zotfileLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
 	zotfileLoader.loadSubScript("chrome://zotfile/content/zotfile.js");
-	
-} 
+}
 
 window.addEventListener('load', function(e) {
 	Zotero.ZotFile.init();
 	// attach focus handler for 'zotero-items-tree' element to check folder for changes
-	window.ZoteroPane.document.getElementById('zotero-items-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
+	window.document.getElementById('zotero-items-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
 }, false);
 
