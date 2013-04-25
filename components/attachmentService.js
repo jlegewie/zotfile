bb5cb2dfd58553ce99db95dcfe7bebe8ead15629
Joscha Legewie <jpl2136@columbia.edu>
2013-04-24 23:22:13 -0400

    fix and linux support for attachment service

--- components/attachmentService.js
+++ components/attachmentService.js
@@ -8,8 +8,9 @@
  * http://mike.kaply.com/2011/01/18/writing-a-firefox-protocol-handler/
  */
 
-// check whether item exists
-// check whether it's a pdf
+// Zotero support of links in notes:
+// https://forums.zotero.org/discussion/15186/clickable-links-in-notes/
+// https://forums.zotero.org/discussion/25832/note-hyperlinks-in-standalone/
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
@@ -41,61 +42,90 @@ zotfileProtocol.prototype = {
       .getService(Components.interfaces.nsISupports)
       .wrappedJSObject;
     var zz = Zotero.ZotFile;
-
-    // zotfile://open/0_EFWJW9U7
+    // exit function
+    var exit = function() {
+      // return channel
+      var channel = ios.newChannelFromURI('about:blank', null);    
+      return channel;
+    }
     // get arguments from uri
+    // e.g. zotfile://open/0_EFWJW9U7
     var [empty, cmd, key, page] = uri.path.substr(1).split('/');
-    if(key) {
-      // open file command
-      if (cmd=='open') {
-        // get zotero item from key
-        var lkh = Zotero.Items.parseLibraryKeyHash(key);
-        if (!lkh) return;
-        var item = Zotero.Items.getByLibraryAndKey(lkh.libraryID, lkh.key);
-        // if attachment, open file and go to page
-        if(item.isAttachment()) {
-          var file = item.getFile();
-          var path = file.path;        
-          // open pdf file
-          zz.runProcess('/usr/bin/open', ['-a', 'Preview', path]);
-          // goto page in pdf file
-          if (page) {
-            if(Zotero.isMac) {
-              var args = [
-                '-e', 'tell app "Preview" to activate', 
-                '-e', 'tell app "System Events" to keystroke "g" using {option down, command down}', 
-                '-e', 'tell app "System Events" to keystroke "' + page + '"',
-                '-e', 'tell app "System Events" to keystroke return']
-              zz.runProcess('/usr/bin/osascript', args, false)
+    // exit if no key
+    if(!key) exit();
+    // open file command
+    if (cmd=='open') {
+      // get zotero item from key
+      var lkh = Zotero.Items.parseLibraryKeyHash(key);
+      if (!lkh) exit();
+      var item = Zotero.Items.getByLibraryAndKey(lkh.libraryID, lkh.key);
+      // if attachment, open file and go to page
+      if(!item.isAttachment()) exit();
+      // get file and path
+      var file = item.getFile();
+      var path = file.path;
+      // check whether pdf file
+      if(path.indexOf('.pdf')==-1) exit();
+      // open pdf and go to page (system specific)
+      if(Zotero.isMac) {
+        // open pdf file
+        zz.runProcess('/usr/bin/open', ['-a', 'Preview', path]);
+        // go to page using applescript
+        var args = [
+          '-e', 'tell app "Preview" to activate', 
+          '-e', 'tell app "System Events" to keystroke "g" using {option down, command down}', 
+          '-e', 'tell app "System Events" to keystroke "' + page + '"',
+          '-e', 'tell app "System Events" to keystroke return']
+        if (page) zz.runProcess('/usr/bin/osascript', args, false)
+      }
+      if(Zotero.isWin) {
+        // path to Adobe Reader
+        // 'C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe'
+        // http://stackoverflow.com/questions/11934159/how-extension-can-read-the-registry
+        var wrk = Components.classes["@mozilla.org/windows-registry-key;1"]
+            .createInstance(Components.interfaces.nsIWindowsRegKey);
+        wrk.open(wrk.ROOT_KEY_LOCAL_MACHINE,
+            "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths",
+            wrk.ACCESS_READ);
+        var acrobat = wrk.readStringValue("AcroRd32.exe");
+        wrk.close();
+        // open pdf file on page
+        zz.runProcess(acrobat, ['/A','"path=' + page + '"', path])
+      }
+      if(Zotero.isLinux) {
+        var cmd = zz.prefs.getCharPref('pdfExtraction.openPdfLinux');
+        // try okular and evince when nothing is set
+        if (cmd=="") {
+          // try okular
+          if (zz.fileExists('/usr/bin/okular')) {
+            zz.runProcess('/usr/bin/okular', ['-p', page, path]);
+          }
+          // try evince
+          else {
+            if (zz.fileExists('/usr/bin/evince')) {
+              zz.runProcess('/usr/bin/evince', ['-p', page, path]);
             }
-            if(Zotero.isWin) {
-              // path to Adobe Reader
-              // 'C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe'
-              // http://stackoverflow.com/questions/11934159/how-extension-can-read-the-registry
-              var wrk = Components.classes["@mozilla.org/windows-registry-key;1"]
-                  .createInstance(Components.interfaces.nsIWindowsRegKey);
-              wrk.open(wrk.ROOT_KEY_LOCAL_MACHINE,
-                  "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths",
-                  wrk.ACCESS_READ);
-              var acrobat = wrk.readStringValue("AcroRd32.exe");
-              wrk.close();
-              // open pdf file on page
-              zz.runProcess(acrobat, ['/A','"path=' + page + '"', path])
-            }
-            if(Zotero.isLinux) {
-              // http://stackoverflow.com/questions/2057595/how-do-you-open-a-pdf-at-a-specific-page-from-the-command-line-osx-or-linux
-              zz.infoWindow('Linux not yet supported','');
-              // which
-              // default for KDE: okular -p 5 file2open.pdf
-              // default for ubonto: evince -p 5 foo.pdf
+            else {
+              zz.infoWindow('Zotfile', 'Zotfile is unable to open the pdf file. Please set the option manually ("pdfExtraction.openPdfLinux" in about:config to something like "/usr/bin/okular -p %(page) %(path)")')
             }
           }
         }
+        // user defined command
+        else {
+          // process option to correct format
+          // e.g. cmd = '/usr/bin/okular -p %(page) %(path)';
+          cmd = cmd.split(' ')
+              .map(function(str) { 
+                  return str_format(str, {'page': page, 'path': path});
+              });
+          // run process
+          var ex = zz.runProcess(cmd[0], cmd.slice(1));
+          if(ex==-1) zz.infoWindow('Zotfile', 'Zotfile is unable to open the pdf file. Please set the option manually ("pdfExtraction.openPdfLinux" in about:config to something like "/usr/bin/okular -p %(page) %(path)")')
+        }
       }
     }
-    // return channel
-    var channel = ios.newChannelFromURI('about:blank', null);    
-    return channel;
+    // exit function
+    exit();
   },
   classDescription: "Zotfile Protocol Handler",
   contractID: "@mozilla.org/network/protocol;1?name=" + "zotfile",
--- defaults/preferences/zotfile.js
+++ defaults/preferences/zotfile.js
@@ -104,6 +104,7 @@ pref("extensions.zotfile.tablet.subfolderFormat", "/%w/%y");
 pref("extensions.zotfile.tablet.mode", 1);
 
 pref("extensions.zotfile.pdfExtraction.UsePDFJS", true);
+pref("extensions.zotfile.pdfExtraction.openPdfLinux", "");
 pref("extensions.zotfile.pdfExtraction.UsePDFJSandPoppler", false);
 pref("extensions.zotfile.pdfExtraction.popplerDeleteTxtFile", true);
 pref("extensions.zotfile.pdfExtraction.Pull", true);
