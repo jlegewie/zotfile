fc515603c50fe8b64269e774732ef8ac006b43c7
Soham Sinha <soham.m17@gmail.com>
2015-02-19 22:12:57 -0700

    Update zotfile.js
    If the destination is just the folder separator then it means that there's no destination mentioned to move the file. So, file will be renamed in-place.

--- chrome/content/zotfile/zotfile.js
+++ chrome/content/zotfile/zotfile.js
@@ -1926,11 +1926,11 @@ Zotero.ZotFile = {
               // create a nslFile Object of the destination folder
               var dir = this.createFile(destination);
               // move file to new location
-              if(destination.trim() == "/")
+              if(destination.trim() == this.folderSep)
 							{
 									//no place to move the file, so rename it in-place
 									this.infoWindow(this.ZFgetString('general.warning'), 
-										'No location is mentioned to move the attachment. File will only be renamed.');
+										'No location is mentioned to move the attachment. File is renamed only.');
 									file.moveTo(null, filename);
 							}
 							else
