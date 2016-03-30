124136f3cf5685890faec98389ba7c3060fa7620
Joscha Legewie <joscha.legewie@gmail.com>
2016-03-29 23:53:03 -0500

    lint include.js

--- chrome/content/zotfile/include.js
+++ chrome/content/zotfile/include.js
@@ -7,16 +7,16 @@ if (!Zotero.ZotFile) {
 }
 
 window.addEventListener('load', function(e) {
-	Zotero.ZotFile.init();
-	if(window.ZoteroPane) {
-		// attach focus handler for 'zotero-items-tree' element to check folder for changes
-		window.ZoteroPane.document.getElementById('zotero-items-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
-		// window.ZoteroPane.document.getElementById('zotero-collections-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
-		// add event listener for zotfile menu items
-		window.ZoteroPane.document.getElementById('zotero-itemmenu').addEventListener('popupshowing', Zotero.ZotFile.showMenu, false);
-		// add event listener for zotfile collection menu
-		window.ZoteroPane.document.getElementById('zotero-collectionmenu').addEventListener('popupshowing', Zotero.ZotFile.showCollectionMenu, false);
-		// add event listener to update saved search for modified tablet attachments
+    Zotero.ZotFile.init();
+    if(window.ZoteroPane) {
+        // attach focus handler for 'zotero-items-tree' element to check folder for changes
+        window.ZoteroPane.document.getElementById('zotero-items-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
+        // window.ZoteroPane.document.getElementById('zotero-collections-tree').addEventListener('focus', Zotero.ZotFile.watchFolder, false);
+        // add event listener for zotfile menu items
+        window.ZoteroPane.document.getElementById('zotero-itemmenu').addEventListener('popupshowing', Zotero.ZotFile.showMenu, false);
+        // add event listener for zotfile collection menu
+        window.ZoteroPane.document.getElementById('zotero-collectionmenu').addEventListener('popupshowing', Zotero.ZotFile.showCollectionMenu, false);
+        // add event listener to update saved search for modified tablet attachments
         window.ZoteroPane.document.getElementById('zotero-collections-tree').addEventListener('click', Zotero.ZotFile.Tablet.updateModifiedAttachmentsSearch, false);
     }
 }, false);
