78ebe50cd4a5ae5bb46dbaf7329aed9efc754976
Dominic Dennenmoser <ddennenmoser@uni-kassel.de>
2022-02-28 15:15:40 +0100

    👌 IMPROVE: Seperat replacing rule

--- chrome/content/zotfile/wildcards.js
+++ chrome/content/zotfile/wildcards.js
@@ -84,9 +84,9 @@ Zotero.ZotFile.Wildcards = new function() {
         }
 
         // replace forbidden characters with meaningful alternatives (they can only apear in the middle of the text at this point)
+        title = title.replace(/<[\w\s"<=\/]+>/g, ''); // Removing rich text formatting
         title = title.replace(/[\/\\]/g, '-');
-        title = title.replace(/<[\w\s"<=\/]+>|[\*|"<>]/g, '');
-        //title = title.replace(/[\*|"<>]/g, '');
+        title = title.replace(/[\*|"<>]/g, '');
         title = title.replace(/[\?:]/g, ' -');
         return(title);
     }
